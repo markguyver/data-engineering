c80eff8c310b7a09ae70965cb43dcafef1c127e7
Mark Chavez <mark.chavez@startmail.com>
2019-06-26 16:48:40 -0600

    Scaffolding basic outline of index functionality

--- application/src/index.ts
+++ application/src/index.ts
@@ -1,17 +1,27 @@
+// Load Dependencies
 import avsc from "avsc";
+import dotenv from 'dotenv';
 import minio from "minio";
 import mongoose from "mongoose";
+
+// Bootstrap Script
+dotenv.config();
+
+// Define Functions to Be Used
 
 const retrieveFromFilesystemDocuments = () => {
 };
 
-const convertDocumentToJsonString = (mongoDocument): string => {
+const convertDocumentToJsonString = (mongoDocument: any): string => {
     return ''; // @todo Delete This
 };
 
-const convertDocumentToAvroString = (mongoDocument): string => {
+const convertDocumentToAvroString = (mongoDocument: any): string => {
     return ''; // @todo Delete This
 };
 
 const saveDocumentToObjectStore = (documentContent: string, bucketName: string, objectPath: string) => {
 };
+
+// Kick Off the Process
+console.log('Data Engineering Script:', process); // @todo Delete This
