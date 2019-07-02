3e42cfeac258ea0224d32248975884b335329cff
Mark Chavez <mark.chavez@startmail.com>
2019-07-02 09:10:59 -0600

    Scaffolding Parquet and MessagePack into functionality

--- application/src/index.ts
+++ application/src/index.ts
@@ -1,12 +1,23 @@
 // Load Dependencies
 import avsc from "avsc";
+import BufferList from "bl";
 import dotenv from "dotenv";
 import minio from "minio";
+import msgpack5 from "msgpack5";
+import parquet from "parquetjs";
 import {IPerson} from "./DataTypes";
 import {generateFakeRecord, generateFakeUuid} from "./fakeDataGenerator"
 
-// Bootstrap Script
+// Bootstrap Script: Load Configuration
 dotenv.config();
+
+// Bootstrap Script: Define Variables
+const minioBucketAvro = process.env.MINIO_BUCKET_AVRO || '';
+const minioBucketJson = process.env.MINIO_BUCKET_JSON || '';
+const minioBucketMessagePack = process.env.MINIO_BUCKET_MESSAGEPACK || '';
+const minioBucketParquet = process.env.MINIO_BUCKET_PARQUET || '';
+
+// Bootstrap Script: Load Imported Modules
 const avroType = avsc.Type.forSchema({
     type: 'record',
     name: 'personObject',
@@ -16,6 +27,7 @@ const avroType = avsc.Type.forSchema({
         {name: 'dateofBirth', type: 'string'}
     ]
 });
+const messagePack = msgpack5();
 const minioClient = new minio.Client({
     endPoint: process.env.MINIO_HOST || '',
     port: 9000,
@@ -23,31 +35,27 @@ const minioClient = new minio.Client({
     accessKey: process.env.MINIO_ACCESS_KEY || '',
     secretKey: process.env.MINIO_SECRET || ''
 });
+const parquetSchema = new parquet.ParquetSchema({
+    firstName: { type: 'UTF8' },
+    lastName: { type: 'UTF8' },
+    dateofBirth: { type: 'UTF8' }
+});
 
-// Define Functions to Be Used
+// Define Non-Exported Functions
 const convertDocumentToJsonString = (personObject: IPerson): string => JSON.stringify(personObject);
 const convertDocumentToAvroString = (personObject: IPerson): Buffer => avroType.toBuffer(personObject);
-const convertDocumentToParquetString = (personObject: IPerson) => {};
-const convertDocumentToMessagePackString = (personObject: IPerson) => {};
-const saveDocumentToObjectStore = (documentContent: string | Buffer, bucketName: string, objectPath: string) => minioClient.putObject(bucketName, objectPath, documentContent);
+const convertDocumentToParquetString = (personObject: IPerson) => '';
+const convertDocumentToMessagePackString = (personObject: IPerson): BufferList => messagePack.encode(personObject);
+const saveDocumentToObjectStore = (documentContent: string | Buffer | BufferList, bucketName: string, objectPath: string) => minioClient.putObject(bucketName, objectPath, documentContent);
 
-const insertIndividualRecordDocuments = (numberOfDocumentsToInsert: number): void => {
+// Define Exported Functions
+export const insertIndividualRecordDocuments = (numberOfDocumentsToInsert: number): void => {
     for (let i = 0; i < numberOfDocumentsToInsert; i++) {
         let currentMockPerson = generateFakeRecord();
         let currentUuid = generateFakeUuid();
-        saveDocumentToObjectStore(convertDocumentToJsonString(currentMockPerson), process.env.MINIO_BUCKET_JSON || '', currentUuid + '.json');
-        saveDocumentToObjectStore(convertDocumentToAvroString(currentMockPerson), process.env.MINIO_BUCKET_AVRO || '', currentUuid + '.avro');
+        saveDocumentToObjectStore(convertDocumentToAvroString(currentMockPerson), minioBucketAvro, currentUuid + '.avro');
+        saveDocumentToObjectStore(convertDocumentToJsonString(currentMockPerson), minioBucketJson, currentUuid + '.json');
+        // saveDocumentToObjectStore(convertDocumentToMessagePackString(currentMockPerson), minioBucketMessagePack, currentUuid + '.messagepack');
+        // saveDocumentToObjectStore(convertDocumentToParquetString(currentMockPerson), minioBucketParquet, currentUuid + '.parquet');
     }
 }
-const insertSingleDocumentWithMultipleRecords = (numberOfRecordsInDocument: number, objectName: string = 'multiple'): void => {
-    let recordBufferObject;
-    for (let i = 0; i < numberOfRecordsInDocument; i++) {
-        let currentMockPerson = generateFakeRecord();
-        if ('undefined' == typeof recordBufferObject) {
-            let recordBufferObject = convertDocumentToAvroString(currentMockPerson);
-        } else {
-            avroType.encode(currentMockPerson, recordBufferObject);
-        }
-    }
-    saveDocumentToObjectStore(recordBufferObject || '', process.env.MINIO_BUCKET_AVRO || '', objectName + '.avro');
-};
